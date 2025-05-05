11c1a7521324efdff14e77785cdb1215db399b29
lemma-85 <rmamuye@chicagobooth.edu>
2025-05-05 17:02:17 +0000

    final update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,26 @@
 class MoviesController < ApplicationController
+
+def update 
+  m_id = params.fetch("the_id")
+
+  matching_records = Movie.where({ :id => m_id })
+  the_movie = matching_records.at(0)
+
+  the_movie.title = params.fetch("the_title")
+  the_movie.year = params.fetch("the_year")
+  the_movie.duration = params.fetch("the_duration")
+  the_movie.description = params.fetch("the_description")
+  the_movie.image = params.fetch("the_image")
+  the_movie.director_id = params.fetch("the_director_id")
+
+  the_movie.save
+
+  redirect_to("/movies/#{the_movie.id}")
+  
+end 
+
 def create
+
   m = Movie.new
   m.title = params.fetch("the_title")
   m.year = params.fetch("the_year")
@@ -14,6 +35,7 @@ def create
 end 
 
 def destroy
+  
 the_id = params.fetch("an_id")
 
 matching_records = Movie.where ({ :id => the_id })
